134c18eaedda4f34939bbd84af36fc32dfc4a1d4
Maxim Lebedev <git@toby3d.me>
2024-08-05 01:58:18 +0500

    :art: Format common strings

--- internal/common/common.go
+++ internal/common/common.go
@@ -2,9 +2,4 @@ package common
 
 const HeaderContentType string = "Content-Type"
 
-const (
-	MIMEApplicationJSON            string = "application/json"
-	MIMEApplicationJSONCharsetUTF8 string = MIMEApplicationJSON + "; " + charsetUTF8
-)
-
-const charsetUTF8 string = "charset=utf-8"
+const MIMEApplicationJSON string = "application/json"
